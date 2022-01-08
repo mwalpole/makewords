4249ce48001c6ae141305daa567652f0444ada50
Mark Walpole <mark.walpole.dev@gmail.com>
2022-01-08 09:49:55 -0500

    add __main__.py to allow debug of module using python -m

---
+++ src/makewords/__main__.py
@@ -0,0 +1,17 @@
+import argparse
+
+from makewords.makewords import possibleWords
+
+
+def main(letters, words):
+    possibleWords(letters, words)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Make words from letters.')
+    parser.add_argument('letters', type=str, nargs='?', default='make')
+    parser.add_argument('words', type=(str), nargs='?', default=None)
+    args = parser.parse_args()
+    letters = args.letters
+    words = args.words.split(',') if args.words is not None else None
+    main(letters, words)
