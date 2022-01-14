4a16c44ef34cf2b0908d377dc27e96b3e6ba0cf2
Mark Walpole <mark.walpole.dev@gmail.com>
2022-01-14 16:48:36 -0500

    reorganize imports from conf and util

--- src/makewords/makewords.py
+++ src/makewords/makewords.py
@@ -1,22 +1,16 @@
 import string
-import textwrap
 
 from nltk.corpus import words as nltklib
 
 import makewords.filters as filters
-from makewords.conf import NLTK_DIR
+import makewords.conf as conf
+import makewords.util as util
 
 import nltk
 
 # os.environ['NLTK_DATA'] = NLTK_DIR not working
 # in the meantime just append the path directly
-nltk.data.path.append(NLTK_DIR)
-
-
-def _print_message(s):
-    """Handle terminal output consistently with NLTK."""
-    prefix = "[makewords] "
-    print(textwrap.fill(s, initial_indent=prefix))
+nltk.data.path.append(conf.NLTK_DIR)
 
 
 def get_clean_words(words=None):
@@ -26,10 +20,10 @@ def get_clean_words(words=None):
     or non-ascii characters.
     """
     if words is None:
-        _print_message("Cleaning 'en' wordlist from nltk.")
+        util.print_message("Cleaning 'en' wordlist from nltk.")
         words = nltklib.words()
     else:
-        _print_message("Using words provided by user.")
+        util.print_message("Using words provided by user.")
     clean_words = set(
         filter(filters.word_does_not_contain_nonascii_lowercase, set(words))
     )
--- src/makewords/util.py
+++ src/makewords/util.py
@@ -1,5 +1,14 @@
+import textwrap
+
+
 def count_letters(word):
     letter_count = {}
     for i in word:
         letter_count[i] = letter_count.get(i, 0) + 1
     return letter_count
+
+
+def print_message(s):
+    """Handle terminal output consistently with NLTK."""
+    prefix = "[makewords] "
+    print(textwrap.fill(s, initial_indent=prefix))
