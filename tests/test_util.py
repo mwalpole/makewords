68ed3c6bd12b33138d594f1271b3862c8f4d2845
Mark Walpole <mark.walpole.dev@gmail.com>
2022-01-14 12:47:11 -0500

    add util tests

---
+++ tests/test_util.py
@@ -0,0 +1,12 @@
+from makewords.filters import word_includes_allowed_letters_only
+from makewords.util import count_letters
+
+
+def test_count_letters():
+    output = count_letters('foobar')
+    assert output == {'f': 1, 'o': 2, 'b': 1, 'a': 1, 'r': 1}
+
+
+def test_count_letters_returns_empty_dict():
+    output = count_letters('')
+    assert output == {}
