e53d44c31a8354e6108f2ac93e498061ff9c5eb9
Mark Walpole <mark.walpole.dev@gmail.com>
2022-01-15 16:38:28 -0500

    init games

---
+++ src/makewords/game/spellingbee.py
@@ -0,0 +1,4 @@
+# Simulate playing NYT Spelling Bee from CLI
+# Choose a word made of seven unique letters and build word corpus
+# Apply rules, identify acceptable guess and pangram
+# Score session
---
+++ src/makewords/game/wordle.py
@@ -0,0 +1,4 @@
+# Simulate playing Wordle from CLI
+# Choose a five letter word and build words corpus
+# Apply rules, identify acceptable guess and provide feedback
+# Score session
