63c5b64db8c7ba9a4d7961844a3adb345f7349c7
Mark Walpole <mark.walpole.dev@gmail.com>
2022-01-27 14:29:55 -0500

    add dockerfile for testing with quay.io

--- setup.py
+++ setup.py
@@ -15,15 +15,20 @@ README = (ROOT_DIR / "README.md").read_text()
 
 setup(
     name="makewords",
+    version="0.1.1",
+    author="Mark Walpole",
+    author_email="mark.walpole.dev@gmail.com",
+    url="https://github.com/mwalpole/makewords",
     long_description=README,
     packages=find_packages("src"),
     package_dir={"": "src"},
     py_modules=[splitext(basename(path))[0] for path in glob("src/*.py")],
+    include_package_data=True,
+    package_data={"": ["src/makewords/nltk_data/*"]},
     entry_points={
         "console_scripts": [
             "makewords=makewords.__main__:main",
             "wordle=makewords.game.wordle:main",
         ]
     },
-    version="0.1.0",
 )
