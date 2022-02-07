b45c04b2e9fd01fc80db7eecb57b6c42ad16ef1a
Mark Walpole <mark.walpole.dev@gmail.com>
2022-02-06 23:55:35 -0500

    add scoring for notebooks

--- src/makewords/score.py
+++ src/makewords/score.py
@@ -1,7 +1,11 @@
+import string
+
 import makewords.util as util
+import makewords.makewords as make
 
 
-def from_words(df, words=None):
+def lfreq(df, words=None):
+    """Return letter frequency."""
     for word in words:
         for i, j in enumerate(word):
             df.loc[j]["total"] += 1  # total count per letter
@@ -10,3 +14,29 @@ def from_words(df, words=None):
         for letter, n in letter_count.items():
             df.loc[letter]["n{}".format(n)] += 1  # count of appearances per word
     return df
+
+
+def top(df, k=5, l=5, m=12, additional=None):
+    """Return top k l-character words based on top m letter frequency.
+    
+    Optionally explicitly include additional words for scoring.
+    """
+    # Take the M most common letters, e.g. ['a', 'e'...]
+    chars = (
+        df['total']
+        .sort_values(axis='index', ascending=False)
+        .head(m)
+        .keys()
+        .to_list()
+    )
+    exclude = set(string.ascii_lowercase).difference(chars)
+    words = make.possible_words(exclude="".join(exclude), length=l)
+    if additional is not None:
+        words = words.union(additional.split(","))
+    scores = {}
+    for word in words:
+        score = 0
+        for i, letter in enumerate(word):
+            score += df.get("z{}".format(i))[letter]
+        scores[word] = score
+    return scores
